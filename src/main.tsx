5cbd8836caf6e179d4fe2b0e18689daacf23f7b8
ricardolcvuba <ricardocontre27@gmail.com>
2025-12-09 18:38:50 -0300

    refactor: reorganize components and update dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { AppProvider } from '@/providers';
+import { AppProvider } from '@/providers/AppProvider';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Router } from './router';
--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,4 @@
-import { Home } from '@/screens';
+import { Home } from '@/screens/home';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 const router = createBrowserRouter([
